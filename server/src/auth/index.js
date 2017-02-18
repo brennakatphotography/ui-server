2e05093c2841266d76e0e7552200c1dd103271c2
Ben Allred <skuttleman@gmail.com>
2017-02-18 10:07:34 -0700

    Create endpoint to logout

--- server/src/auth/index.js
+++ server/src/auth/index.js
@@ -1,12 +1,16 @@
 const route = require('express').Router();
 const { buildQueryString } = require('../utils/url');
 const { PHOTO_API } = process.env;
-const REDIRECT_PATH = '/auth/callback';
 
 route.get('/login', ({ headers: { host }, protocol }, response, next) => {
-  let redirect_uri = `${protocol}://${host}${REDIRECT_PATH}`;
+  let redirect_uri = `${protocol}://${host}/auth/callback`;
   let query = buildQueryString({ redirect_uri });
   response.redirect(`${PHOTO_API}/auth/login${query}`);
+});
+
+route.get('/logout', (request, response, next) => {
+  response.clearCookie('token', { httpOnly: true });
+  response.redirect('/');
 });
 
 route.get('/callback', ({ query: { token } }, response, next) => {
