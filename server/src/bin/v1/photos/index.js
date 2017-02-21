987a1dfcbce0339217c720850f3bd4f16d7e53ca
Ben Allred <skuttleman@gmail.com>
2017-02-21 16:03:45 -0700

    Stream files from api directly instead of storing in temp folder

--- server/src/bin/v1/photos/index.js
+++ server/src/bin/v1/photos/index.js
@@ -1,14 +1,14 @@
 const route = require('express').Router();
-const { unlink } = require('fs');
-const { streamFile, sendFile } = require('../../../utils/file');
+const request = require('request');
 const { buildQueryString } = require('../../../utils/url');
+const { PHOTO_API } = process.env;
+const makeHeaders = require('../../../utils/makeHeaders');
 
 route.get('/:id', ({ cookies, params, query, schema }, response, next) => {
-  let url = `/bin/v1/photos/${params.id}${buildQueryString(query)}`;
-  streamFile(url, cookies.token)
-    .then(sendFile(response))
-    .then(unlink)
-    .catch(next);
+  const { token: Authorization } = cookies;
+  const url = `${PHOTO_API}/bin/v1/photos/${params.id}${buildQueryString(query)}`;
+  request(url, makeHeaders({ Authorization }))
+    .pipe(response);
 });
 
 module.exports = route;
